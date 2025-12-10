0490f399b2b8477758141de4cb3b1ed95b41d2c1
Delfina Cañas <79326402+delfigica@users.noreply.github.com>
2025-12-10 09:37:07 -0300

    build static for gh pages

--- docs/_next/static/MXkqZlt32J1VDCTpSLpBy/_buildManifest.js
+++ docs/_next/static/MXkqZlt32J1VDCTpSLpBy/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/crosstime//_next/:path+",
+        "destination": "/crosstime/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
